6050f794580945293f9c6bd90d9b62ba9f077e09
Imtiaj-Sajin <imtiajsajin@gmail.com>
2025-02-06 18:16:05 +0600

    push force

--- src/leads/lead.entity.ts
+++ src/leads/lead.entity.ts
@@ -1,52 +1,52 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
+  import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
 
-@Entity()
-export class Lead {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Entity()
+  export class Lead {
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({ nullable: true })
-  email: string;
+    @Column({ nullable: true })
+    email: string;
 
-  @Column()
-  ip: string;
+    @Column()
+    ip: string;
 
-  @Column({ nullable: true })
-  city: string;
+    @Column({ nullable: true })
+    city: string;
 
-  @Column({ nullable: true })
-  regionName: string;
+    @Column({ nullable: true })
+    regionName: string;
 
-  @Column({ nullable: true })
-  latitude: string;
+    @Column({ nullable: true })
+    latitude: string;
 
-  @Column({ nullable: true })
-  longitude: string;
+    @Column({ nullable: true })
+    longitude: string;
 
-  @Column({ nullable: true })
-  country: string;
+    @Column({ nullable: true })
+    country: string;
 
-  @Column({ nullable: true })
-  timezone: string;
+    @Column({ nullable: true })
+    timezone: string;
 
-  @Column({ nullable: true })
-  currency: string;
+    @Column({ nullable: true })
+    currency: string;
 
-  @Column({ type: 'boolean', nullable: true })  
-  mobile: boolean;
+    @Column({ type: 'boolean', nullable: true })  
+    mobile: boolean;
 
-  @Column({ nullable: true })
-  device: string;
+    @Column({ nullable: true })
+    device: string;
 
-  @Column()
-  source: string;
+    @Column()
+    source: string;
 
-  @Column({ nullable: true })
-  userAgent: string;
+    @Column({ nullable: true })
+    userAgent: string;
 
-  @Column({ nullable: true })
-  referrer: string;
+    @Column({ nullable: true })
+    referrer: string;
 
-  @CreateDateColumn()
-  createdAt: Date;
-}
+    @CreateDateColumn()
+    createdAt: Date;
+  }
